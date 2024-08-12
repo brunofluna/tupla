1822123fa0156d4a7199bbd77433f5ea8ab0cb3d
brunofluna <bruno@brunoluna.com.br>
2024-08-12 14:58:26 -0300

    V 1.0

---
+++ main.py
@@ -0,0 +1,22 @@
+# tupla
+paises = ('Brasil', 'Paraguai', 'Romênia', 'França', 'Espanha', 'Portugal', 'Argentina', 'China', 'Japão', 'Tailândia', 'Itália')
+
+# exibe a tupla
+for pais in paises:
+    print(pais)
+
+print('\n')
+
+# exibe o tipo de coleção
+print(type(paises))
+print('\n')
+
+# joga a tupla em uma lista e ordena
+paises_lista = sorted(paises)
+
+# exibe a lista
+for pais in paises_lista:
+    print(pais)
+
+print('\n')
+print(type(paises_lista))
